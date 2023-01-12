838e82129641f84f92d0b5ae5f285406f07324bf
dtuan <dtuanlieb@gmail.com>
2023-01-12 11:14:34 +0100

    increment price

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { timer } from 'rxjs';
 import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   title = 'ScooTeq';
   price = 0
+  ngOnInit(){
+    this.incrementPrice()
+  }
+  incrementPrice(){
+    this.price += 0.15;
+    // console.log("this.price ", this.price.toFixed(2))
+
+    setTimeout(() => {
+      this.incrementPrice()
+
+    }, 1000);
+    // setTimeout(this.incrementPrice,2000);
+  
+  }
 }
